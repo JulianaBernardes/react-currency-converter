383a1f21054983e250250571faa2c0eedeaaa1de
Juliana Bernardes Pereira <julianabp96@gmail.com>
2021-07-30 15:36:00 +0100

    update key

--- src/components/Converter.js
+++ src/components/Converter.js
@@ -15,14 +15,14 @@ export default class Converter extends Component {
 
     toConvert(){
         
+        const key = process.env.REACT_APP_API_KEY
         let from_to = `${this.props.currencyA}_${this.props.currencyB}`;
-        const url = `/.netlify/functions/currency?from_to=${from_to}`;
+        let url = `https://free.currconv.com/api/v7/convert?q=${from_to}&compact=ultra&apiKey=${key}`
 
         fetch(url).then(res=>{
             return res.json()
         })
         .then(json=> {
-            console.log(json[from_to])
             let quotation = json[from_to]
             let currencyB_value = (parseFloat(this.state.currencyA_value) * quotation).toFixed(2)
             this.setState({currencyB_value})
